72e1ff269a89341eb5a84093607fc4bc5c9d4544
Shiva Gupta <shivag22@yahoo.co.in>
2026-08-03 13:09:32 +0530

    feat: Integrating reset button functionallity into the tictactoe component

--- src/component/TicTacToe.tsx
+++ src/component/TicTacToe.tsx
@@ -1,10 +1,17 @@
+import { useTicTacToe } from '../hooks/useTicTacToe';
+import { ResetButton } from './ResetButton';
 import { BOARD_TITLE } from '../constants';
+import './component.css';
 
 export default function TicTacToe() {
+  const {
+    handleReset,
+  } = useTicTacToe();
 
   return (
     <div className="tic-tac-toe">
       <h2>{BOARD_TITLE}</h2>
+      <ResetButton onReset={handleReset} />
     </div>
   );
 }
--- src/tests/TicTacToe.test.tsx
+++ src/tests/TicTacToe.test.tsx
@@ -1,2 +1,34 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import TicTacToe from '../component/TicTacToe';
+import {
+  RESET_LABEL,
+  PLAYER_X,
+} from '../constants';
+
 describe('TicTacToe Integration', () => {
+
+  test('resets game state through ResetButton', () => {
+    render(<TicTacToe />);
+    const squares = screen.getAllByRole('button', { name: /square/i });
+
+    fireEvent.click(squares[0]);
+    expect(squares[0]).toHaveTextContent(PLAYER_X);
+
+    fireEvent.click(screen.getByRole('button', { name: new RegExp(RESET_LABEL, 'i') }));
+    expect(squares[0]).toHaveTextContent('');
+  });
+
+  test('resets board and status after a win', () => {
+    render(<TicTacToe />);
+    const squares = screen.getAllByRole('button', { name: /square/i });
+
+    fireEvent.click(squares[0]);
+    fireEvent.click(squares[3]);
+    fireEvent.click(squares[1]);
+    fireEvent.click(squares[4]);
+    fireEvent.click(squares[2]);
+
+    fireEvent.click(screen.getByRole('button', { name: new RegExp(RESET_LABEL, 'i') }));
+    expect(screen.getAllByRole('button', { name: /square/i })).toHaveLength(9);
+  });
 });
